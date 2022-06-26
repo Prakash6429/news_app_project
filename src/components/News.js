5a07deaf751bd8ce3ec6a3bbc1c69f295a7c68cf
Prakash Raj <rajprakash19.may@gmail.com>
2022-06-26 22:24:22 +0530

    video 31

--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
 
 export class News extends Component {
   
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general'
+  } 
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+
   constructor() {
     super();
     console.log("Hello I am a constructor from news component");
@@ -16,7 +29,7 @@ export class News extends Component {
 
   async componentDidMount() {
     let url =
-      `https://newsapi.org/v2/top-headlines?country=in&apiKey=691719ab95ae4565b36d6e2605b4cbf2&page=1&pageSize=${this.props.pageSize}`;
+      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=691719ab95ae4565b36d6e2605b4cbf2&page=1&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -30,7 +43,7 @@ export class News extends Component {
     console.log("Previous");
 
     let url =
-    `https://newsapi.org/v2/top-headlines?country=in&apiKey=691719ab95ae4565b36d6e2605b4cbf2&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=691719ab95ae4565b36d6e2605b4cbf2&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
   let data = await fetch(url);
   let parsedData = await data.json();
@@ -48,7 +61,7 @@ export class News extends Component {
     if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
 
       let url =
-      `https://newsapi.org/v2/top-headlines?country=in&apiKey=691719ab95ae4565b36d6e2605b4cbf2&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=691719ab95ae4565b36d6e2605b4cbf2&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
       this.setState({loading:true});
       let data = await fetch(url);
       let parsedData = await data.json();
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,8 +6,8 @@ export class NewsItem extends Component {
     let {title, description,imageUrl,newsUrl} = this.props;
     return (
     <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl?imageUrl:"https://cricketaddictor.com/wp-content/uploads/2022/06/Indian-Cricket-Team-1024x768.jpeg"} className="card-img-top" height="200px" alt="..."/>
+        <div className="card">
+            <img src={imageUrl?imageUrl:"https://cricketaddictor.com/wp-content/uploads/2022/06/Indian-Cricket-Team-1024x768.jpeg"} className="card-img-top" height="270px" alt="..."/>
             <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description?description:"Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com"}...</p>
