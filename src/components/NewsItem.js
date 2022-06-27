9ef6fb674e2cae20cd689b9dd7c645cf78008a3f
Prakash Raj <rajprakash19.may@gmail.com>
2022-06-27 11:19:27 +0530

    commit

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
   
   render() {
-    let {title, description,imageUrl,newsUrl} = this.props;
+    let {title, description,imageUrl,newsUrl, author, date,source} = this.props;
     return (
-    <div className='my-3'>
+    <div className='my-3 mx-1'>
         <div className="card">
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex:'1'}}>{source}</span>
             <img src={imageUrl?imageUrl:"https://cricketaddictor.com/wp-content/uploads/2022/06/Indian-Cricket-Team-1024x768.jpeg"} className="card-img-top" height="270px" alt="..."/>
             <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">{title}</h5>
             <p className="card-text">{description?description:"Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com"}...</p>
+            <p className="card-text"><small class="text-muted">By {author?author:"anonymous"} on {new Date(date).toGMTString()}</small></p>
             <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
