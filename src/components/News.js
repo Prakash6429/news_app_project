9d690e3055df352dff61eba26f44a338ef346593
Prakash Raj <rajprakash19.may@gmail.com>
2022-06-27 12:27:43 +0530

    stage

--- src/components/News.js
+++ src/components/News.js
@@ -17,19 +17,24 @@ export class News extends Component {
     category: PropTypes.string
   }
 
-  constructor() {
-    super();
+  capitalizeFirstLetter = (string)=> {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props) {
+    super(props);
     console.log("Hello I am a constructor from news component");
     this.state = {
       articles: [],
       loading: false,
       page: 1
     };
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
 
-  async componentDidMount() {
-    let url =
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe4b75f5c31748d4a55bc30ac4b52baa&page=1&pageSize=${this.props.pageSize}`;
+  async updateNews(){
+    const url =
+      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe4b75f5c31748d4a55bc30ac4b52baa&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -39,45 +44,62 @@ export class News extends Component {
           loading:false });
   }
 
+  async componentDidMount() {
+    // let url =
+    //   `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe4b75f5c31748d4a55bc30ac4b52baa&page=1&pageSize=${this.props.pageSize}`;
+    // this.setState({loading:true});
+    // let data = await fetch(url);
+    // let parsedData = await data.json();
+    // console.log(parsedData);
+    // this.setState({ articles: parsedData.articles,
+    //       totalResults: parsedData.totalResults,
+    //       loading:false });
+    this.updateNews();
+  }
+
   handlePrevClick = async()=>{
     console.log("Previous");
 
-    let url =
-    `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe4b75f5c31748d4a55bc30ac4b52baa&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true});
-  let data = await fetch(url);
-  let parsedData = await data.json();
-  console.log(parsedData);
+  //   let url =
+  //   `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe4b75f5c31748d4a55bc30ac4b52baa&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+  //   this.setState({loading:true});
+  // let data = await fetch(url);
+  // let parsedData = await data.json();
+  // console.log(parsedData);
 
-    this.setState({
-      page:this.state.page - 1,
-      articles: parsedData.articles,
-      loading:false
-    })
+  //   this.setState({
+  //     page:this.state.page - 1,
+  //     articles: parsedData.articles,
+  //     loading:false
+  //   })
+    this.setState({page:this.state.page - 1});
+    this.updateNews();
   }
 
   handleNextClick = async()=>{
     console.log("Next");
-    if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
+  //   if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
 
-      let url =
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe4b75f5c31748d4a55bc30ac4b52baa&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-      this.setState({loading:true});
-      let data = await fetch(url);
-      let parsedData = await data.json();
+  //     let url =
+  //     `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe4b75f5c31748d4a55bc30ac4b52baa&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+  //     this.setState({loading:true});
+  //     let data = await fetch(url);
+  //     let parsedData = await data.json();
 
-      this.setState({
-        page:this.state.page + 1,
-        articles: parsedData.articles,
-        loading:false
-      })
-  }
+  //     this.setState({
+  //       page:this.state.page + 1,
+  //       articles: parsedData.articles,
+  //       loading:false
+  //     })
+  // }
+  this.setState({page:this.state.page + 1})
+  this.updateNews();
   }
 
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center my-3" >NewsMonkey - Top Headlines</h1>
+        <h1 className="text-center my-3" >NewsMonkey - Top Headlines from {this.capitalizeFirstLetter(this.props.category)}</h1>
         {this.state.loading && <Spinner/>}
         <div className="row">
           {!this.state.loading && this.state.articles.map((element) => {
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,12 +7,12 @@ export class NewsItem extends Component {
     return (
     <div className='my-3 mx-1'>
         <div className="card">
-        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex:'1'}}>{source}</span>
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex:'1'}}>{source}</span>
             <img src={imageUrl?imageUrl:"https://cricketaddictor.com/wp-content/uploads/2022/06/Indian-Cricket-Team-1024x768.jpeg"} className="card-img-top" height="270px" alt="..."/>
             <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description?description:"Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com"}...</p>
-            <p className="card-text"><small class="text-muted">By {author?author:"anonymous"} on {new Date(date).toGMTString()}</small></p>
+            <p className="card-text"><small className="text-muted">By {author?author:"anonymous"} on {new Date(date).toGMTString()}</small></p>
             <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
